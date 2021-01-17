ab3ac0a9dccc1b0b234a5fb5245d5035642b05b1
Main丶侯 <1440248234@qq.com>
2021-01-17 19:08:55 +0800

    ✨ add config mode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from '../user/user.controller';
+import { ConfigModule } from '@nestjs/config';
+import config from 'config';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, // 作用于全局
+      load: [config], // 加载自定义配置项
+    }),
+  ],
   controllers: [AppController, UserController],
   providers: [AppService],
 })
