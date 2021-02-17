3f28033e9b0612a6cc09b12f9cab750a608ca13e
MainHou <1440248234@qq.com>
2021-02-17 23:06:29 +0800

    ⬆️ upgrade vite,ant-design-vue

--- web/vite.config.js
+++ web/vite.config.js
@@ -7,9 +7,10 @@ import path from 'path';
  */
 export default defineConfig({
   plugins: [vue()],
-  alias: {
-    // 路径映射必须以/开头和结尾
-    '@': path.resolve(__dirname, 'src/'),
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
   },
   server: {
     // proxy: {
