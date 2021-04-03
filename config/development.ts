56d2a10bb1821f70d4dc87af28864f6a469a10f6
houyusheng <1440248234@qq.com>
2021-04-03 22:42:15 +0800

    ✨ fix bug,commit sql file

---
+++ config/development.ts
@@ -0,0 +1,23 @@
+export default {
+  // 端口
+  port: parseInt(process.env.PORT, 10) || 3000,
+  // 是否开启swagger
+  enableSwagger: true,
+  // 数据库配置
+  DATABASE_CONFIG: {
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'root',
+    password: 'root',
+    database: 'forum',
+    timezone: 'UTC',
+    charset: 'utf8mb4',
+    entities: ['./**/*.entity.js'],
+    synchronize: true,
+    logging: true,
+  },
+  jwtConstants: {
+    secret: 'secret',
+  },
+};
---
+++ config/index.ts
@@ -0,0 +1,10 @@
+import developmentConfig from './development';
+import testConfig from './test';
+import productionConfig from './production';
+const configs = {
+  development: developmentConfig,
+  test: testConfig,
+  production: productionConfig,
+};
+const env = process.env.NODE_ENV || 'development';
+export default () => configs[env];
