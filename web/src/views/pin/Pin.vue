153839056bde474e99e5edaa0075e5f520ea4b0d
houyusheng <1440248234@qq.com>
2021-04-10 18:31:18 +0800

    ✨func add comment

--- web/src/views/pin/Pin.vue
+++ web/src/views/pin/Pin.vue
@@ -1,10 +1,11 @@
 <template>
     <div id="pins">
+        评论列表
         <a-list
             class="comment-list"
-            :header="`${data.length} replies`"
+            :header="`${comments.length} replies`"
             item-layout="horizontal"
-            :data-source="data"
+            :data-source="comments"
         >
             <template #renderItem="{ item }">
                 <a-list-item>
@@ -24,36 +25,89 @@
                 </a-list-item>
             </template>
         </a-list>
+        <a-comment>
+            <template #avatar>
+                <a-avatar
+                    src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png"
+                    alt="Han Solo"
+                />
+            </template>
+            <template #content>
+                <a-form-item>
+                    <a-textarea :rows="4" v-model:value="value" />
+                </a-form-item>
+                <a-form-item>
+                    <a-button
+                        html-type="submit"
+                        :loading="submitting"
+                        type="primary"
+                        @click="handleSubmit"
+                    >Add Comment</a-button>
+                </a-form-item>
+            </template>
+        </a-comment>
     </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
 import moment from 'moment';
 
 export default defineComponent({
     name: 'Pin',
     setup() {
-        return {
-            data: [
-                {
-                    actions: ['回复'],
+        const comments = ref<any[]>([]);
+        const submitting = ref<boolean>(false);
+        const value = ref<string>('');
+
+        const data = [
+            {
+                actions: ['回复'],
+                author: 'Han Solo',
+                avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                content:
+                    'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+                datetime: moment().subtract(1, 'days'),
+            },
+            {
+                actions: ['回复'],
+                author: 'Han Solo',
+                avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
+                content:
+                    'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+                datetime: moment().subtract(2, 'days'),
+            },
+        ];
+
+        comments.value.push(...data);
+
+        const handleSubmit = () => {
+            if (!value.value) {
+                return;
+            }
+
+            submitting.value = true;
+
+            setTimeout(() => {
+                submitting.value = false;
+                comments.value.push({
                     author: 'Han Solo',
                     avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-                    content:
-                        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-                    datetime: moment().subtract(1, 'days'),
-                },
-                {
-                    actions: ['回复'],
-                    author: 'Han Solo',
-                    avatar: 'https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png',
-                    content:
-                        'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
+                    content: value.value,
                     datetime: moment().subtract(2, 'days'),
-                },
-            ],
+                });
+                value.value = '';
+                console.log(comments.value);
+            }, 1000);
+        };
+
+        return {
+            comments,
+            submitting,
+            value,
+
             moment,
+            handleSubmit,
         };
     },
 })
